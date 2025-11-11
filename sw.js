56117f16270fbb9b65cb1d8c8bfef2bdad9061ef
juano30 <60760607+juano30@users.noreply.github.com>
2025-11-11 01:17:18 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
 const CACHE_NAME = 'pae-multiforms-cache-v2';
-const ASSETS = ['./', './index.html', './admin.html', './style.css', './db.js', './app.js', './admin.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './assets/logo-pae.png', './assets/logo-org.png'];
+const ASSETS = ['./', './index.html', './admin.html', './style.css', './db.js', './app.js', './admin.js', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './assets/logo-pae.png', './assets/logo-org.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
